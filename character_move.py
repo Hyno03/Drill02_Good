5891cf14f4614f1699e0f660bdf2c73e7c6caad0
Hyno03 <jenny2117@naver.com>
2023-09-20 18:12:55 +0900

    Add render_all(x,y)
    render_all(x,y)라는 함수 생성 후 이미지를 띄우는 코드를 작성 후 함수를 다른 코드에 적용

--- character_move.py
+++ character_move.py
@@ -6,30 +6,32 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_all(x, y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.01)
+    
 def run_circle():
 
     cx, cy, r = 800 / 2, 600 / 2, 200
     for deg in range(0, 360, 5):
         x = cx + r * math.cos(deg / 360 * 2 * math.pi)
         y = cy + r * math.sin(deg / 360 * 2 * math.pi)
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.01)
+        render_all(x, y)
 
 
 def run_rectangle():
     #bottom line
     for x in range(50, 750 + 1, 10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x, 90)
-        delay(0.01)
-    pass
+        render_all(x, 90)
+
+    for x in range(750 , 50, -10):
+        pass
 
 while True:
-    #run_circle()
-    run_rectangle()
+    run_circle()
+    #run_rectangle()
     break
 
 close_canvas()
