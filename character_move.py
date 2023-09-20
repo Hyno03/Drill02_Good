a1980e7528b5de918ca1594eb4618132248d80ac
Hyno03 <jenny2117@naver.com>
2023-09-20 17:42:38 +0900

    Make a big frame of code
    함수를 이용하여 프로그램의 큰 틀을 구성

---
+++ character_move.py
@@ -0,0 +1,22 @@
+from pico2d import*
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def run_circle():
+    print('Circle')
+    pass
+
+def run_rectangle():
+    print('Rectangle')
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
+
+close_canvas()
